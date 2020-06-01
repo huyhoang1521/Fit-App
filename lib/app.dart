0aaf0fb2103111f56b4d4bbc765ec5d0893f8e42
Huy Hoang <hhoang2197@gmail.com>
2020-06-01 17:41:16 -0500

    firebase database initialized

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Fit App",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Fit App"),
+            ),
+            body: Center(child: Text("Hello World!"))));
+  }
+}
