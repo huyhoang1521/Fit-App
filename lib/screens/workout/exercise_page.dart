632747c51f1d4fa90cbc5e7b19b61c4ae1cf8ed7
Your Name <nicholaspurdum@gmail.com>
2021-01-07 10:25:19 -0600

    Pulling

--- lib/screens/workout/exercise_page.dart
+++ lib/screens/workout/exercise_page.dart
@@ -96,13 +96,6 @@ class _ExercisePageState extends State<ExercisePage> {
             enabled: pressed,
             rWPressed: () {},
             fFPressed: () {
-              //Navigator.push(context,
-              //    RouteTransition(page: RestPage(), start: start, end: end));
-              //Todo Clean up after choosing transition method
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => RestPage()),
-              // );
               exerciseCounter.incrementExerciseCount();
               Navigator.pushNamed(context, '/restPage');
             },
